b82ab781538ef95102b5eb9214bc8e050fe8b518
Amil Masic <amil.masic@gmail.com>
2018-08-26 18:48:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,7 @@ def input_to_index(user_input)
   input = user_input .to_i - 1
 end
 def valid_move?(board, index)
-  if position_taken?(board, index) || board[index] == nil
+  if !position_taken?(board, index) || board[index] == nil
       false
     else board[index] == index.between?(0,8)
       true
