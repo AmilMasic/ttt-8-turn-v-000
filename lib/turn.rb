8650ae167c701c8b4c71970ca6fee8b8a74d70db
Amil Masic <amil.masic@gmail.com>
2018-08-26 18:50:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,9 +14,5 @@ def valid_move?(board, index)
   end
 end
 def position_taken? (board, index)
-  if board[index] == "X" || board[index] == "O"
-    true
-  else
-    false
-  end
+ board[index] == "X" || board[index] == "O"
 end
