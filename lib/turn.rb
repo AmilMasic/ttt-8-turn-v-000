64c38e45d45262781601314b005eba37b6dfbba4
Amil Masic <amil.masic@gmail.com>
2018-08-26 18:47:45 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+def display_board(board = ["    ", "    ", "    ", "   ", "   ", "   ", "   ", "   ", "   "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  input = user_input .to_i - 1
+end
+def valid_move?(board, index)
+  if position_taken?(board, index) || board[index] == nil
+      false
+    else board[index] == index.between?(0,8)
+      true
+
+end
+end
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? (board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
